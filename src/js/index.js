baf553e50bccef3445d8a69c52682562deec4a66
Faouzi <l.a.-noire@hotmail.com>
2019-02-13 14:15:17 +0100

    ajout de la fonction pour changer de couleur (a ameliorer), changement de certains textes et amelioration de la navbar

---
+++ src/js/index.js
@@ -0,0 +1,49 @@
+// CHANGEMENT DE COULEUR
+// VARIABLES
+let blanc = document.getElementsByClassName('btn btn-light')[0]
+let noir = document.getElementsByClassName('btn btn-dark')[0]
+let h1 = document.getElementsByTagName('h1')
+let a = document.querySelectorAll('#section-trois a')
+let nav = document.getElementsByTagName('nav')[0]
+let aNav = document.querySelectorAll('#navbarSupportedContent a')
+let body = document.getElementsByTagName('body')[0]
+
+// FONCTION POUR PASSER AU NOIR
+let bgNoir = () =>{
+    body.classList.add('bg-noir')
+    nav.classList.add('bg-noir')
+
+    for(let i = 0; i<h1.length; i++){
+        h1[i].classList.add('text-blanc')
+    }
+
+    for(let i = 0; i< a.length; i++){
+        a[i].classList.add('text-blanc')
+    }
+
+    for(let i = 0; i< aNav.length; i++){
+        aNav[i].classList.add('text-blanc')
+    }
+}
+
+// FONCTION POUR PASSER AU BLANC
+let bgBlanc = () =>{
+    body.classList.remove('bg-noir')
+    nav.classList.remove('bg-noir')
+
+    for(let i = 0; i<h1.length; i++){
+        h1[i].classList.remove('text-blanc')
+    }
+
+    for(let i = 0; i< a.length; i++){
+        a[i].classList.remove('text-blanc')
+    }
+
+    for(let i = 0; i< aNav.length; i++){
+        aNav[i].classList.remove('text-blanc')
+    }
+}
+
+// ECOUTEURS D'EVENEMENTS
+noir.addEventListener('click',bgNoir)
+blanc.addEventListener('click',bgBlanc)
